24bd97ceaa21b24022793743d6849a8b6d118e03
mnmonherdene <mnmonherdene1234@gmail.com>
2022-12-31 13:10:44 +0800

    wow working nice2

--- Pages/Employees/Index.cshtml.cs
+++ Pages/Employees/Index.cshtml.cs
@@ -1,16 +1,23 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPages.Models;
+using RazorPages.Services;
 
 namespace RazorPages.Pages.Employees;
 
 public class IndexModel : PageModel
 {
+    private readonly IEmployeeRepository employeeRepository;
     public string Message { get; set; }
-    public IndexModel()
+    public IEnumerable<Employee>? Employees { get; set; }
+
+    public IndexModel(IEmployeeRepository employeeRepository)
     {
+        this.employeeRepository = employeeRepository;
         Message = "Hello World";
     }
+
     public void OnGet()
     {
-
+        Employees = employeeRepository.GetEmployees();
     }
 }
